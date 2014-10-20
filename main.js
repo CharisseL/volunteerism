407ca03150abf9ecba83797cfdb6ab919de3c32b
CharisseL <charisse8@gmail.com>
2014-10-20 15:41:14 -0500

    teams of three & random pairing added

--- main.js
+++ main.js
@@ -9,29 +9,69 @@ function addItemToList($list, itemText){
 	$li.innerHTML = itemText;
 	$list.appendChild($li);
 }
+
+function neighborGrouping(list, groupSize, target){
+	var listClone = list.slice(0);
+				while (listClone.length > 0) {
+					var listItems = listClone.splice(0, groupSize);
+					addItemToList(target, listItems.join(' &amp; '));
+				}
+}
+
+function arrayShuffle(array){
+	var arrayClone = array.slice(0);
+	var temp;
+	for (var i = 0; i < arrayClone.length; i++){
+		var rand = getRandomInt(0, arrayClone.length);
+		temp = arrayClone[i];
+		arrayClone[i] = arrayClone[rand];
+		arrayClone[rand] = temp;
+	}
+	return arrayClone;
+
+}
+
+function show(element){
+	element.classList.remove('hidden');						
+}
+
 //add eventListener..what happens on the click
 document.addEventListener('DOMContentLoaded', function(){
   var $form = document.getElementById('generate-group');
 	var students = ['charisse', 'evan', 'greg','jessica', 'luke', 'gerald', 'adam', 'sonda', 'beck', 'leon'];
-//
+
+	var $select = $form.querySelector('select');
+	var $numBox = $form.querySelector('input [type='number']');
+	$select.addEventListener('change', function (event){
+		if (event.currentTarget.value === 'randN'){
+			show($numBox);
+			}else {
+				hide($numBox);
+			}
+		});
+
 	$form.addEventListener('submit', function(event){
 		event.preventDefault();
 		var $ul = document.getElementById('results');
 		$ul.innerHTML = '';				//print the name in an ul in the DOM
 
 		var groupCriteria = $form.querySelector('select').value;
-		
 		if(groupCriteria === 'random-student'){		//if random-student is selected, then 
-				var studentNumber = getRandomInt (0, students.length);		//run this & pick random student [i] 
-				var studentName = students[studentNumber]; 	//student name will be name in [index position]
-				addItemToList($ul, studentName);
+			var studentNumber = getRandomInt (0, students.length);		//run this & pick random student [i] 
+			var studentName = students[studentNumber]; 	//student name will be name in [index position]
+			addItemToList($ul, studentName);
 		} else if (groupCriteria === 'neighbor-pairing') {
-				var studentsClone = students.slice(0);
-				while ( studentsClone.length > 0) {
-					var studentNames = studentsClone.splice(0, 2);
-					addItemToList($ul, studentNames.join(' &amp; '));
-				}
-			}
+				neighborGrouping(students, 2, $ul);
+
+		} else if (groupCriteria === 'team-three') {
+				neigborGrouping(students, 3, $ul);
+		}else if(groupCriteria === 'randPair'){
+			var shuffledStudents = arrayShuffle(students);
+			neighborGrouping(shuffledStudents, 2, $ul);
+		}else if(groupCriteria === 'randN'){
+
+		}
+			
 	});
 });
 	
